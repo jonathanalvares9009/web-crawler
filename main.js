2bcd93b7fc3985cb783cc8b1fe15ebea7c7d3fc3
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-10-22 09:56:04 +0530

    Implemented YC scraping

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { crawlPage } = require("./crawl");
 const { printReport, saveReport } = require("./report");
+const fs = require('fs');
 
 async function main() {
   if (process.argv.length < 3) {
@@ -10,13 +11,32 @@ async function main() {
     console.log("Too many arguments");
     process.exit(1);
   }
-  const baseURL = process.argv[2];
-  const pages = await crawlPage(baseURL, baseURL, {});
 
-  const fileName = new URL(baseURL).hostname.split('.')[0];
-  saveReport(fileName, pages);
+  if (process.argv[2] === "--yc-companies") {
+    fs.readFile('output/yc_companies.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const urls = data.split('\n');
+      urls.forEach(async (url) => {
+        if (url === '') {
+          return;
+        }
+        const pages = await crawlPage(url, url, {});
+        const fileName = new URL(url).hostname.split('.')[0];
+        saveReport(fileName, pages);
+        printReport(pages);
+      });
+    });
+  } else {
+    const baseURL = process.argv[2];
+    const pages = await crawlPage(baseURL, baseURL, {});
 
-  printReport(pages);
+    const fileName = new URL(baseURL).hostname.split('.')[0];
+    saveReport(fileName, pages);
+    printReport(pages);
+  }
 }
 
 main();
